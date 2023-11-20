ad01a1f33fae6eb28c4fabff59808b77875180e7
Gino Fabricante <ginofabricante@Ginos-MBP-2.lan>
2023-11-20 01:09:09 -0800

    added Code Optimization assessment

---
+++ Code_Optimization/test_j.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include "test_j.h"
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
+// gcc -lm test_j.c -o test_j
+
+double fptildemin, aX, gX, fpt, alpha, gamma1, sigma_a, sigma_b, exp1arg, sigma, exp2arg;
+
+void calculateConstants()
+{
+    fptildemin = (1.0/2.0/M_PI) * pow((4.0 * b / 5.0), (1.0/4.0));
+    aX  = (log(a)-log(aC))/log(fptildemin);
+    gX  = -log(gC)/log(fptildemin);
+}
+
+double function_j(double f, double fp, double fptilde)
+{
+  fpt = MAX(fptilde, fptildemin);
+
+  alpha   = aC  * pow(fpt, aX);
+  gamma1   = gC  * pow(fpt, gX);
+  sigma_a = saC * pow(fpt, saX);
+  sigma_b = sbC * pow(fpt, sbX);
+  exp1arg = -1.25 * pow((f/fp),-4);
+  sigma   = (f <= fp) * sigma_a + (f > fp) * sigma_b;
+
+  exp2arg = -0.5 * pow((f-fp)/(sigma*fp), 2);
+
+  return alpha * pow(g, 2) * pow((2*M_PI), -4) * pow(f,-5) * exp(exp1arg) * pow(gamma1, exp(exp2arg));
+}
+
+int main()
+{
+  clock_t startTime = clock();
+
+  double S, f, fp, fptilde;
+
+  calculateConstants();
+
+  for (f = -5.; f <= 5.; f += 0.01)
+  {
+    for (fp = 0.; fp <= 10.; fp += 0.01)
+    {
+      for (fptilde = 0.; fptilde <= 10.; fptilde += 0.01)
+      {
+        S = function_j(f, fp, fptilde);
+      }
+    }
+  }
+
+  clock_t endTime = clock();
+  double elapsedTime = ((double)(endTime - startTime)) / CLOCKS_PER_SEC;
+
+  printf("Elapsed time: %f seconds\n", elapsedTime);
+  return 0;
+}
---
+++ Code_Optimization/test_j.h
@@ -0,0 +1,21 @@
+#ifndef TEST_J_H_
+#define TEST_J_H_
+
+
+const double a  = 0.0081;
+const double b  = 0.6;
+const double g  = 9.807;
+const double gC = 5.87;
+const double aC = 0.0317;
+const double saC = 0.0547;
+const double saX = 0.32;
+const double sbC = 0.0783;
+const double sbX = 0.16;
+
+void calculateConstants();
+
+double function_j(double f,
+                  double fp,
+                  double fptilde);
+
+#endif // TEST_J_H_
