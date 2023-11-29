770e22cca03366d0e420618e0da0437d5c093a43
Gino Fabricante <ginofabricante@Ginos-MBP-2.lan>
2023-11-28 16:17:13 -0800

    added clarity to input var

--- Coordinate_Transformation/CCU.c
+++ Coordinate_Transformation/CCU.c
@@ -13,18 +13,18 @@ static double rad2deg(double rad_in)
   return rad_in * (180.0 / M_PI);
 }
 
-static void checkLat(double lat)
+static void checkLat(double lat_in)
 {
-  if (lat < -90.0 || lat > 90.0)
+  if (lat_in < -90.0 || lat_in > 90.0)
   {
     fprintf(stderr, "Error: Latitude is not in the specified range [-90.0, 90.0].\n");
     exit(EXIT_FAILURE);
   }
 }
 
-static void checkLon(double lat)
+static void checkLon(double lat_in)
 {
-  if (lat < -180.0 || lat > 180.0)
+  if (lat_in < -180.0 || lat_in > 180.0)
   {
     fprintf(stderr, "Error: Longitude is not in the specified range [-180.0, 180.0].\n");
     exit(EXIT_FAILURE);
@@ -34,15 +34,15 @@ static void checkLon(double lat)
 
 // bearing function is meant to have radians inputted since it is used internally
 // and GIS2Radar() & RtoG() already initially convert to radians, makes sense to convert once
-static double bearingFunc(double lat1, double lon1, double lat2, double lon2)
+static double bearingFunc(double lat1_in, double lon1_in, double lat2_in, double lon2_in)
 {
   double deltaLon, Y, X, temp;
 
-  deltaLon = lon2 - lon1;
+  deltaLon = lon2_in - lon1_in;
 
-  Y = cos(lat2) * sin(deltaLon);
-  X = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(deltaLon);
-  
+  Y = cos(lat2_in) * sin(deltaLon);
+  X = cos(lat1_in) * sin(lat2_in) - sin(lat1_in) * cos(lat2_in) * cos(deltaLon);
+
   temp = rad2deg(atan2(Y, X));
 
   // make sure it is within [0.0, 360.0]
@@ -54,48 +54,48 @@ static double bearingFunc(double lat1, double lon1, double lat2, double lon2)
   return temp;
 }
 
-void GIS2Radar(double lat1, double lon1, double lat2, double lon2, double *range, double *bearingInit, double *bearingFinal)
+void GIS2Radar(double lat1_in, double lon1_in, double lat2_in, double lon2_in, double *range_in, double *bearingInit_in, double *bearingFinal_in)
 {
   double deltaLat, deltaLon, A, B;
 
   // check inputs are within valid ranges
-  checkLat(lat1);
-  checkLon(lon1);
-  checkLat(lat2);
-  checkLon(lon2);
+  checkLat(lat1_in);
+  checkLon(lon1_in);
+  checkLat(lat2_in);
+  checkLon(lon2_in);
 
   // convert to radians
-  deltaLat = deg2rad(lat2 - lat1);
-  deltaLon = deg2rad(lon2 - lon1);
-  lat1 = deg2rad(lat1);
-  lon1 = deg2rad(lon1);
-  lat2 = deg2rad(lat2);
-  lon2 = deg2rad(lon2);
+  deltaLat = deg2rad(lat2_in - lat1_in);
+  deltaLon = deg2rad(lon2_in - lon1_in);
+  lat1_in = deg2rad(lat1_in);
+  lon1_in = deg2rad(lon1_in);
+  lat2_in = deg2rad(lat2_in);
+  lon2_in = deg2rad(lon2_in);
 
   // calculate constants for equation
   A = deltaLat / 2.0;
   B = deltaLon / 2.0;
 
-  *range = 2 * EARTH_RADIUS * asin(sqrt( (sin(A) * sin(A)) + (sin(B) * sin(B)) * cos(lat1) * cos(lat2) )); // haversine equation
-  *bearingInit = bearingFunc(lat1, lon1, lat2, lon2);
-  *bearingFinal = bearingFunc(lat2, lon2, lat1, lon1) - 180;
+  *range_in = 2 * EARTH_RADIUS * asin(sqrt( (sin(A) * sin(A)) + (sin(B) * sin(B)) * cos(lat1_in) * cos(lat2_in) )); // haversine equation
+  *bearingInit_in = bearingFunc(lat1_in, lon1_in, lat2_in, lon2_in);
+  *bearingFinal_in = bearingFunc(lat2_in, lon2_in, lat1_in, lon1_in) - 180;
 }
 
-void RtoG (double range, double bearingInit, double lat1, double lon1, double *lat2, double *lon2, double *bearingFinal)
+void RtoG (double range_in, double bearingInit_in, double lat1_in, double lon1_in, double *lat2_in, double *lon2_in, double *bearingFinal_in)
 {
   // check inputs are within valid ranges
-  checkLat(lat1);
-  checkLon(lon1);
+  checkLat(lat1_in);
+  checkLon(lon1_in);
 
   // convert to radians
-  bearingInit = deg2rad(bearingInit);
-  lat1 = deg2rad(lat1);
-  lon1 = deg2rad(lon1);
+  bearingInit_in = deg2rad(bearingInit_in);
+  lat1_in = deg2rad(lat1_in);
+  lon1_in = deg2rad(lon1_in);
 
   // we use the calculated lat2 and lon2 for calculating bearingFinal
-  *lat2 = rad2deg(asin(sin(lat1) * cos(range/EARTH_RADIUS) + cos(lat1) * sin(range/EARTH_RADIUS) * cos(bearingInit)));
-  *lon2 = rad2deg(lon1 + atan2(sin(bearingInit) * sin(range/EARTH_RADIUS) * cos(lat1), cos(range/EARTH_RADIUS) - sin(lat1) * sin(deg2rad(*lat2))));
-  *bearingFinal = bearingFunc(deg2rad(*lat2), deg2rad(*lon2), lat1, lon1) - 180;
+  *lat2_in = rad2deg(asin(sin(lat1_in) * cos(range_in/EARTH_RADIUS) + cos(lat1_in) * sin(range_in/EARTH_RADIUS) * cos(bearingInit_in)));
+  *lon2_in = rad2deg(lon1_in + atan2(sin(bearingInit_in) * sin(range_in/EARTH_RADIUS) * cos(lat1_in), cos(range_in/EARTH_RADIUS) - sin(lat1_in) * sin(deg2rad(*lat2_in))));
+  *bearingFinal_in = bearingFunc(deg2rad(*lat2_in), deg2rad(*lon2_in), lat1_in, lon1_in) - 180;
 }
 
 // Driver code for demonstration
